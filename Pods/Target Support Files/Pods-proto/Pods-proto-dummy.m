1f37d8ec10ce8ec17aab8194b64079390e656bf0
Alexander Murphy <amurphy345@gmail.com>
2016-10-26 11:35:50 -0600

    setting up pods and adding firebase

---
+++ Pods/Target Support Files/Pods-proto/Pods-proto-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_proto : NSObject
+@end
+@implementation PodsDummy_Pods_proto
+@end
---
+++ Pods/Target Support Files/Pods-protoTests/Pods-protoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_protoTests : NSObject
+@end
+@implementation PodsDummy_Pods_protoTests
+@end
