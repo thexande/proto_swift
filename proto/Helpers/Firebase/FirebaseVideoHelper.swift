f31e7505e393dc2c8d50b9ffcea8d6ba025a55e6
Alexander Murphy <amurphy345@gmail.com>
2016-11-08 21:17:19 -0700

    before using synx

---
+++ proto/Helpers/Firebase/FirebaseVideoHelper.swift
@@ -0,0 +1,13 @@
+//
+//  VideoHelper.swift
+//  proto
+//
+//  Created by Alexander Murphy on 11/8/16.
+//  Copyright © 2016 Alexander Murphy. All rights reserved.
+//
+
+import Foundation
+
+class FirebaseVideoHelper {
+    
+}
